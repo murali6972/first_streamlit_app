c5e21f0cc7e8f802367a77acddee9f2b14f00274
murali6972 <46634900+murali6972@users.noreply.github.com>
2023-06-12 12:57:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,22 +42,21 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 
-#streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
 
-# write your own comment - what does this do?
-
-streamlit.stop()
-
-# import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
-
+#Sbowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.couser() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+    
+#Add a button to load the fruit
+if streamlit.botton('get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
